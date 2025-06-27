9cb50cf602d31907dfcb55311171371b5b8cba18
UnidentifiedAccount <tanthaddeo5@gmail.com>
2025-06-27 01:51:39 +0000

    keys

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -26,6 +26,11 @@ export class Game extends Scene {
         this.ball=this.physics.add.image(width/2, height/2, 'ball').setScale(0.05, 0.05).refreshBody();
         this.ball.setCollideWorldBounds(true);
         this.ball.setBounce(1,1);
+        this.cursors=this.input.keyboard.createCursorKeys();
+        this.wasd=this.input.keyboard.addKeys({
+            up:Phaser.Input.Keyboard.KeyCodes.W
+            down:Phaser.Input.Keyboard.KeyCodes.S
+        });
     
 
         this.input.keyboard.on('keydown-SPACE', this.StartBall, this);
